9463013e8a9528d37926e988f5808efd65c7e48c
kachDev7 <99486031+kachDev7@users.noreply.github.com>
2022-06-30 20:51:20 +0100

    my commit

--- routes/post.js
+++ routes/post.js
@@ -8,25 +8,6 @@ const Post = require('../models/Post');
 // Init Router
 const router = express.Router();
 
-
-// Set up Multer Storage for images
-// const storage = multer.diskStorage({
-//     // image destination
-//     destination: (req, file, cb) => {
-//         cb(null, './public/uploads/images/')
-//     },
-//     // image name
-//     filename: (req, file, cb) => {
-//         cb(null,  Date.now() + file.originalname)
-//     }
-// })
-
-// //  Multer upload  setup
-// const upload = multer({
-//     storage: storage
-// })
-
-
 // Shannels get request from DB. Schema : Post
 router.get('/', async (req, res) => {
     try{
@@ -50,25 +31,6 @@ router.get('/:postId', async (req, res) => {
 
 // Shannels Post Request
 router.post('/', async (req, res) => {
-
-
-    //  upload.array("image1"),
-    // save images to Cloudinary and return the URLs and IDs
-    // const uplaoder = async (path) => await cloudinary.uploads(path, "sha-images");
-    // const urls = []
-    // const files = req.files;
-    // for(const file of files){
-    //     const { path } = file;
-    //     const newPath = await uplaoder(path);
-    //     urls.push(newPath);
-    //     fs.unlinkSync(path)
-    // }
-    // console.log(urls)
-    // const image1 = urls[0].url
-    // const image2 = urls[1].url
-    
-
-
     // set vlalues from req Post Schema
     const post = new Post({
         title: req.body.title,
@@ -79,11 +41,10 @@ router.post('/', async (req, res) => {
         image2: req.body.image_two,
         author: req.body.author,
         category: req.body.category
-    })
+    });
     try {
         const savedPost = await post.save();
         res.json({"message": "Post Completed!"});
-        console.log(savedPost)
     } catch (err) {
         return({ message : err});
     }
